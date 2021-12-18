5f27a5951d9be58ac099010bf97d5a5eb4617bda
Ehi <ehinomen.c@vaptales.com>
2021-12-18 16:38:34 -0500

    updated projects

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,6 +2,10 @@ import './card.css'
 import { data } from '../project/data'
 import { GitHub, Link } from '@material-ui/icons'
 import portfolio from '../../images/portfolio-website-image.png'
+import array from '../../images/sorting-visualizer-image.png'
+import backend from '../../images/backend-social-media.png'
+
+
 
 const Card = () => {
   return (
@@ -9,14 +13,11 @@ const Card = () => {
       <div className='projectWrapper'>
         <div className='projectOne'>
           <div className='imageBox'>
-            <img src={portfolio} alt='' />
+            <img src={array} alt='' />
             <div className='overlay'>
-              <div className='imageName'>Personal Portfolio Website</div>
+              <div className='imageName'>Array Sorting Visualizer</div>
               <p className='imageDesc'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                ut quas officiis numquam rem minima, delectus natus eligendi
-                velit doloribus possimus fugiat. Ad consectetur dignissimos
-                dolores vitae totam sunt qui?
+                This is a react project with a very simple ui that allows users to see the various sorting algorithms and how they work in real time. It features popular sorting algorithms like bubble sort, selection sort, merge sort, quick sort and insertion sort. 
               </p>
             </div>
           </div>
@@ -24,38 +25,72 @@ const Card = () => {
             <h3 className='techStackTitle'>{`//Tech stack`}</h3>
             <ul className='techStackList'>
               <li className='techList'>ReactJS</li>
-              <li className='techList'>ReactJS</li>
+              
             </ul>
           </div>
           <div className='codeLinks'>
             <h3 className='codeLinksTitle'>{`//See it in action`}</h3>
             <ul className='codeLinksList'>
+              <a href='https://sortingvisualizerehi.netlify.app/' target='blank'>
               <li className='links'>
                 Visit the site <Link className='icons' />
               </li>
+              </a>
+              <a href='https://github.com/Leehis/Sorting-Visualizer' target='blank'>
               <li className='links'>
                 See the code <GitHub className='icons' />
               </li>
+              </a>
+
             </ul>
           </div>
         </div>
+
         <div className='projectTwo'>
           <div className='imageBox'>
-            <img src={portfolio} alt='' />
+            <img src={backend} alt='' />
             <div className='overlay'>
-              <div className='imageName'>Personal Portfolio Website</div>
+              <div className='imageName'>Social Media Backend</div>
               <p className='imageDesc'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                ut quas officiis numquam rem minima, delectus natus eligendi
-                velit doloribus possimus fugiat. Ad consectetur dignissimos
-                dolores vitae totam sunt qui?
+                This is the code for a social media backend. It allows users to follow each other, create posts, log-in, create accounts and many other social interactions. All this information is also stored in a database.
               </p>
             </div>
           </div>
           <div className='techStack'>
             <h3 className='techStackTitle'>{`//Tech stack`}</h3>
             <ul className='techStackList'>
-              <li className='techList'>ReactJS</li>
+              <li className='techList'>nodeJs</li>
+              <li className='techList'>expressJS</li>
+              <li className='techList'>mongoose</li>
+
+            </ul>
+          </div>
+          <div className='codeLinks'>
+            <h3 className='codeLinksTitle'>{`//See it in action`}</h3>
+            <ul className='codeLinksList'>
+             
+              <a href='https://github.com/Leehis/social_media_backend' target='blank'>
+                <li className='links'>
+                  See the code <GitHub className='icons' />
+                </li>
+              </a>
+            </ul>
+          </div>
+        </div>
+
+        <div className='projectThree'>
+          <div className='imageBox'>
+            <img src={portfolio} alt='' />
+            <div className='overlay'>
+              <div className='imageName'>Personal Website</div>
+              <p className='imageDesc'>
+                This is the website you're currently on. This website is built with the purpose of showing off the work I have done.
+              </p>
+            </div>
+          </div>
+          <div className='techStack'>
+            <h3 className='techStackTitle'>{`//Tech stack`}</h3>
+            <ul className='techStackList'>
               <li className='techList'>ReactJS</li>
             </ul>
           </div>
@@ -64,7 +99,7 @@ const Card = () => {
             <ul className='codeLinksList'>
               <a href=''>
                 <li className='links'>
-                  Visit the site <Link className='icons' />
+                  Visit the site? You're already here! <Link className='icons' />
                 </li>
               </a>
               <a href='https://github.com'>
